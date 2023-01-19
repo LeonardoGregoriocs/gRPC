6c6173e3448e48aed49985b7a2370026169e0f82
Leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-01-18 23:24:35 -0300

    Implementando servidor gRPC

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/LeonardoGregoriocs/gRPC/internal/database"
+	"github.com/LeonardoGregoriocs/gRPC/internal/pb"
+	"github.com/LeonardoGregoriocs/gRPC/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()                                // Cria o servidor gRPC.
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService) // Conexão entre o servidor (gRPC) e serviço.
+	reflection.Register(grpcServer)
+
+	// Abrindo conexão TCP para conseguimos nos comunicar com o gRPC.
+
+	lis, err := net.Listen("tcp", ":50051") // 50051 -> Porta padrão do gRPC.
+	if err != nil {
+		panic(err)
+	}
+
+	// Inicialização do servidor gRPC.
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
